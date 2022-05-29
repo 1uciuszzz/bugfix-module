c4ed94509a7bf7b8ddc4499069cbf6c4ab832dd8
tankMan <tbrt1373@126.com>
2022-05-29 23:45:12 +0800

    git stash pop

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ import cors from "cors";
 import "./database.js";
 import user from "./routers/user.js";
 import auth from "./routers/auth.js";
+import bug from "./routers/bug.js"
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 server.use("/api/v1/user", user);
 server.use("/api/v1/auth", auth);
+server.use("/api/v2/bug", bug);
 
 server.listen(8000, () => {
   console.log("服务器启动在端口8000");
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import NotFound from "../views/NotFound.vue";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import DeveloperView from "../views/DeveloperView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
@@ -21,6 +21,20 @@ const router = createRouter({
       path: "/developer",
       name: "developer",
       component: DeveloperView,
+    },
+    {
+      path:"/manager",
+      name:"manager",
+      component:()=>import("../views/ManagerView.vue"),
+      children:[{
+        path:"",
+        name:"",
+        component:()=>import("../components/PersonManage.vue")
+      },{
+        path:"person",
+        name:"person",
+        component:()=>import("../components/PersonManage.vue")
+      }]
     },
     {
       path: "/notfound",
